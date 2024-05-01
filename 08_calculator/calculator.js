b95cf53645a61d76775f9ecc0547651998540bb9
darianmorat <yojhandariantoledomora@gmail.com>
2024-05-01 18:41:57 -0500

    Calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,35 +1,36 @@
-const add = function() {
-	
+const add = function (a, b) {
+   return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+   return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+   return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+   return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+   return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+   if (n === 0) {
+      return 1;
+   }
+   return n * factorial(n - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+   add,
+   subtract,
+   sum,
+   multiply,
+   power,
+   factorial,
 };
-
-// test
