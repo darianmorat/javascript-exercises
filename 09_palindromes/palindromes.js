fd27474f823fd81e508ffeb34b980d349cb7807f
darianmorat <yojhandariantoledomora@gmail.com>
2024-05-02 07:48:00 -0500

    Palindrome completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+   const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+   let stringCleaned = string
+      .toLowerCase()
+      .split("")
+      .filter((character) => alphanumerical.includes(character))
+      .join("");
+
+   let stringReversed = stringCleaned.split("").reverse().join("");
+
+   return stringCleaned === stringReversed;
 };
 
 // Do not edit below this line
